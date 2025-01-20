c6750d60129c7ee7204358a67846a9efa19026d2
Maycon Douglas <mdrs.maycon@gmail.com>
2025-01-20 19:31:13 -0300

    feat: ai prediction to Reform

--- divide.ai-reform/src/interfaces/IAIPrediction.ts
+++ divide.ai-reform/src/interfaces/IAIPrediction.ts
@@ -6,15 +6,19 @@ export interface ExpenseByCategory {
 }
   
 export interface IAIPrediction {
-    nextExpenses: number | null;
-    nextIncome: number | null;
-    nextExpensesByCategory: ExpenseByCategory[];
-    recomendation: string;
-    response: string;
-    hasAnalysis: Boolean | null;
+  response: string;
+}
+
+export interface ChatResponse {
+  prompt: string;
+  response: string;
+  userId: number;
+  groupId: number;
+  createdAt: string;
 }
 
 export interface IAIPredictionRequest {
   userId: number;
-  prompt: string;
+  groupId: number;
+  prompt?: string | null;
 }
